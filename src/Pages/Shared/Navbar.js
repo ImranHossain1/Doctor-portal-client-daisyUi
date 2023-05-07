efee4c44b8226bffe4ea8715ee353b089fd0088a
ImranHossain1 <imranhossain1402@gmail.com>
2023-05-07 22:06:48 +0200

    navbar updated

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -12,10 +12,10 @@ const Navbar = () => {
     }
     const menuItems = <>
                     <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
+                    {/* <li><Link to='/about'>About</Link></li> */}
                     <li><Link to='/appointment'>Appointment</Link></li>
-                    <li><Link to='/review'>Reviews</Link></li>
-                    <li><Link to='/contact'>Contact Us</Link></li>
+                    {/* <li><Link to='/review'>Reviews</Link></li> */}
+                    {/* <li><Link to='/contact'>Contact Us</Link></li> */}
                     {
                         user &&  <li><Link to='/dashboard'>Dashboard</Link></li>
                     }
